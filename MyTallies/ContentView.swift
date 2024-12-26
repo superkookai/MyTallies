2ce8e05919820efc6ffb27c28d81ba02311dc47a
Weerawut <weerawut@live.com>
2024-12-26 10:05:08 +0700

    Initial Commit

---
+++ MyTallies/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyTallies
+//
+//  Created by Weerawut Chaiyasomboon on 26/12/2567 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyTallies/MyTalliesApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyTalliesApp.swift
+//  MyTallies
+//
+//  Created by Weerawut Chaiyasomboon on 26/12/2567 BE.
+//
+
+import SwiftUI
+
+@main
+struct MyTalliesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
